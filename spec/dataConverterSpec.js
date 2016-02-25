716f3e512d896f8355c92e598d5e0a228a7801d0
Pekka <pekka.tukiainen@philips.com>
2016-02-25 11:12:40 +0200

    refactored dataconverterspec

--- spec/dataConverterSpec.js
+++ spec/dataConverterSpec.js
@@ -31,95 +31,66 @@ describe("DataConverterFactory", function() {
 });
 
 describe("Tcx2Converter", function() {
-    it("should return correct start time", function() {
+    
+    beforeEach(function() {
         var relativePath = 'data/993568829.tcx';
         var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.startTime).toBe("2015-11-30T21:51:29.000+02:00");
+        this.route = converter.createConverter(buffer).convert(buffer);
+    });    
+
+    it("should return correct start time", function() {
+        expect(this.route.startTime).toBe("2015-11-30T21:51:29.000+02:00");
     });
 
     it("should return correct device", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.device).toBe("Fitbit Surge");
+        expect(this.route.device).toBe("Fitbit Surge");
     });
     
     it("should return correct amount of track points", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints.length).toBe(2502);
+        expect(this.route.trackPoints.length).toBe(2502);
     });
     
     it("should return correct time stamp", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[4].timeStamp).toBe("2015-11-30T21:51:43.000+02:00");
+        expect(this.route.trackPoints[4].timeStamp).toBe("2015-11-30T21:51:43.000+02:00");
     });
 
     it("should return correct altitude", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[10].altitude).toBe("8.1");
+        expect(this.route.trackPoints[10].altitude).toBe("8.1");
     });
 
     it("should return correct distance", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[2501].distance).toBe("5438.1");
+        expect(this.route.trackPoints[2501].distance).toBe("5438.1");
     });
 
     it("should return correct heart rate", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[2499].heartRate).toBe("137");
+        expect(this.route.trackPoints[2499].heartRate).toBe("137");
     });
 
     it("should return correct position", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[2498].lat).toBe(60.18599021434784);
-        expect(route.trackPoints[2498].lng).toBe(25.05650023619334);
+        expect(this.route.trackPoints[2498].lat).toBe(60.18599021434784);
+        expect(this.route.trackPoints[2498].lng).toBe(25.05650023619334);
     });
     
     it("should return correct starting position", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.startLat).toBe(60.18615917364756);
-        expect(route.startLng).toBe(25.056412513057392);
+        expect(this.route.startLat).toBe(60.18615917364756);
+        expect(this.route.startLng).toBe(25.056412513057392);
     });
 
     it("should return correct total duration", function() {
-        var relativePath = 'data/993568829.tcx';
+        expect(this.route.duration).toBe("41:42"); // 22:33:11 - 21:51:29
+
+        var relativePath = 'data/1671940524.tcx';
         var buffer = readFileAsString(relativePath);
         var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.duration).toBe("41:42"); // 22:33:11 - 21:51:29
-
-        relativePath = 'data/1671940524.tcx';
-        buffer = readFileAsString(relativePath);
-        route = converter.createConverter(buffer).convert(buffer);
         expect(route.duration).toBe("60:28"); // 22:56:06 - 21:55:38
     });
 
     it("should return correct incremental duration", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.trackPoints[3].duration).toBe("0:13"); // 21:51:29 - 21:51:42
+        expect(this.route.trackPoints[3].duration).toBe("0:13"); // 21:51:29 - 21:51:42
     });
     
     it("should return correct distance", function() {
-        var relativePath = 'data/993568829.tcx';
-        var buffer = readFileAsString(relativePath);
-        var route = converter.createConverter(buffer).convert(buffer);
-        expect(route.distance).toBe("5438.1");
+        expect(this.route.distance).toBe("5438.1");
     });
     
 });
