c3fc5eedbc113f56cc58bbb93ef3f2c18be53df0
Pekka <pekka.tukiainen@philips.com>
2016-02-29 23:37:45 +0200

    adding velocity info

--- public/controller.js
+++ public/controller.js
@@ -66,7 +66,7 @@ kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', funct
             lng: $scope.route.data.startLng,
             zoom: 15
         };
-        var totalTimeInHours = runTime($scope.route.data.trackPoints[0].timeStamp, $scope.route.data.trackPoints[trackPoints.length-1].timeStamp);
+        var totalTimeInHours = runTimeInHours($scope.route.data.trackPoints[0].timeStamp, $scope.route.data.trackPoints[trackPoints.length-1].timeStamp);
         $scope.totalVelocity = lengthInKilometers($scope.route.data.distance) / totalTimeInHours;
         console.log($scope.route);
     }
