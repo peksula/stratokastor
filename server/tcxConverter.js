15b4858cf66c0cc2ced58bb185ed48b44f4f5f31
Pekka Tukiainen <pekka.tukiainen@philips.com>
2016-11-21 21:42:00 +0200

    cleaned up tcxconverter and it tests

--- server/tcxConverter.js
+++ server/tcxConverter.js
@@ -1,36 +1,31 @@
-var utils = require('./utils');
-var Promise = require("bluebird");
-var xml2js = Promise.promisifyAll(require('xml2js'));
-
+var utils = require('./utils')
+var Promise = require("bluebird")
+var xml2js = Promise.promisifyAll(require('xml2js'))
 
 exports.convert = function (data) {
 
     function route(startTime, device, dataPoints, duration, distance, climb, startLat, startLng, kmh, minkm, geoPoints) {
-        this.startTime = startTime;
-        this.device = device;
-        this.dataPoints = dataPoints;
-        this.duration = duration;
-        this.distance = distance;
-        this.climb = climb;
-        this.startLat = startLat;
-        this.startLng = startLng;
-        this.kmh = kmh;
-        this.minkm = minkm;
-        this.geoPoints = geoPoints;
+        this.startTime = startTime
+        this.device = device
+        this.dataPoints = dataPoints
+        this.duration = duration
+        this.distance = distance
+        this.climb = climb
+        this.startLat = startLat
+        this.startLng = startLng
+        this.kmh = kmh
+        this.minkm = minkm
+        this.geoPoints = geoPoints
     }
- 
+
     var heartRateAtTrackPoint = function(trackPoint) {
         if (trackPoint.HeartRateBpm === undefined) {
-            return "";
+            return "" // not all devices supply heart rate info, return empty string in that case
         }
-        return trackPoint.HeartRateBpm.Value;
+        return trackPoint.HeartRateBpm.Value
     }
 
     var createDataPoint = function(tcxPoint, startTime, totalClimb, totalDistance, previousPoint) {
-        var currentLatLon = {
-            lat: parseFloat(tcxPoint.Position.LatitudeDegrees),
-            lng: parseFloat(tcxPoint.Position.LongitudeDegrees)
-       };
         var point = {
             timeStamp: tcxPoint.Time,
             altitude: tcxPoint.AltitudeMeters,
@@ -41,104 +36,93 @@ exports.convert = function (data) {
             climb: totalClimb,
             heartRate: heartRateAtTrackPoint(tcxPoint),
             percentage: utils.percentageRun(tcxPoint.DistanceMeters, totalDistance),
-        };
-        if (previousPoint !== undefined) {
-            distanceSinceLastPoint = parseFloat(point.distance) - parseFloat(previousPoint.distance);
-            point.kmh = utils.kmh(distanceSinceLastPoint, utils.durationInHours(previousPoint.timeStamp, point.timeStamp));
-            point.minkm = utils.minkm(distanceSinceLastPoint, utils.durationInMinutes(previousPoint.timeStamp, point.timeStamp));
         }
-
-        return point;
+        if (previousPoint !== undefined) {
+            distanceSinceLastPoint = parseFloat(point.distance) - parseFloat(previousPoint.distance)
+            point.kmh = utils.kmh(distanceSinceLastPoint, utils.durationInHours(previousPoint.timeStamp, point.timeStamp))
+            point.minkm = utils.minkm(distanceSinceLastPoint, utils.durationInMinutes(previousPoint.timeStamp, point.timeStamp))
+        }
+        return point
     }
 
     var createGeoPoint = function(tcxPoint) {
         var geoPoint = {
             lat: parseFloat(tcxPoint.Position.LatitudeDegrees),
             lng: parseFloat(tcxPoint.Position.LongitudeDegrees)
-            };
-        return geoPoint;
+            }
+        return geoPoint
     }
-    
+
     var getStartLatLon = function(laps) {
-        var lap = laps[0];
-        var firstTrackPoint = lap.Track.Trackpoint[0];
+        var lap = laps[0] // assumes laps are used, todo: don't assume.
+        var firstTrackPoint = lap.Track.Trackpoint[0]
         return {
             lat: parseFloat(firstTrackPoint.Position.LatitudeDegrees),
             lng: parseFloat(firstTrackPoint.Position.LongitudeDegrees)
-        };
+        }
     }
-    
+
     var getTotalDistance = function(laps) {
-        for (i=laps.length; i > 0; i--) {
-            var lap = laps[i-1];
-            for (j=lap.Track.Trackpoint.length; j > 0; j--) {
-                var trackPoint = lap.Track.Trackpoint[j-1];
+        // Could just get the last DistanceMeters value, but there are
+        // cases where DistanceMeters zeroes during the run. So this gets
+        // last non-zero value.
+        for (i = laps.length; i > 0; i--) {
+            var lap = laps[i-1]
+            for (j = lap.Track.Trackpoint.length; j > 0; j--) {
+                var trackPoint = lap.Track.Trackpoint[j-1]
                 if (parseFloat(trackPoint.DistanceMeters) > 0) {
-                    return Math.round(trackPoint.DistanceMeters * 100) / 100;
+                    return Math.round(trackPoint.DistanceMeters * 100) / 100
                 }
             }
         }
-        return 0;
+        return 0
     }
-    
+
     var getStartTime = function(laps) {
-        for (i=0; i < laps.length; i++) {
-            var lap = laps[i];
-            for (j=0; j < lap.Track.Trackpoint.length; j++) {
-                var trackPoint = lap.Track.Trackpoint[j];
-                return trackPoint.Time;
-            }
-        }
+        return laps[0].Track.Trackpoint[0].Time
     }
-    
+
     var getEndTime = function(dataPoints) {
-        return dataPoints[dataPoints.length-1].timeStamp;
+        return dataPoints[dataPoints.length-1].timeStamp
     }
 
     var objectToArray = function(object) {
         if (!Array.isArray(object)) {
-            var array = [];
-            array.push(object);
-            return array;
+            var array = []
+            array.push(object)
+            return array
         }
-        return object;
+        return object
     }
-    
+
     var processJson = function(json) {
-        
-        var laps = objectToArray(json.TrainingCenterDatabase.Activities.Activity.Lap);
-
-        var dataPoints = [];
-        var geoPoints = [];
-        var lastAltitudeReading;
-        var previousPoint;
-        var totalClimb = 0;    
-        var totalDistance = getTotalDistance(laps);
-        var startTime = getStartTime(laps);
-        var startLatLon = getStartLatLon(laps);
-
+        var laps = objectToArray(json.TrainingCenterDatabase.Activities.Activity.Lap)
+        var dataPoints = []
+        var geoPoints = []
+        var lastAltitudeReading
+        var previousPoint
+        var totalClimb = 0
+        var totalDistance = getTotalDistance(laps)
+        var startTime = getStartTime(laps)
 
         laps.forEach(function(lap) {
-            var trackPoints = objectToArray(lap.Track.Trackpoint);
+            var trackPoints = objectToArray(lap.Track.Trackpoint)
             trackPoints.forEach(function(trackPoint) {
                 if (trackPoint.Position !== undefined) {
-                    
                     // Only create data points if there is lat/lon information available
-                    totalClimb += utils.climbSinceLastPoint(trackPoint.AltitudeMeters, lastAltitudeReading);
-                    lastAltitudeReading = parseFloat(trackPoint.AltitudeMeters);
-
-                    var point = createDataPoint(trackPoint, startTime, totalClimb, totalDistance, previousPoint);
-                    dataPoints.push(point);
-                    var geoPoint = createGeoPoint(trackPoint);
-                    geoPoints.push(geoPoint);
-                    previousPoint = point;
+                    totalClimb += utils.climbSinceLastPoint(trackPoint.AltitudeMeters, lastAltitudeReading)
+                    lastAltitudeReading = parseFloat(trackPoint.AltitudeMeters)
+                    var point = createDataPoint(trackPoint, startTime, totalClimb, totalDistance, previousPoint)
+                    dataPoints.push(point)
+                    var geoPoint = createGeoPoint(trackPoint)
+                    geoPoints.push(geoPoint)
+                    previousPoint = point
                 }
-            });
-        });            
-            
-            
-        var endTime = getEndTime(dataPoints);
-        
+            })
+        })
+
+        var endTime = getEndTime(dataPoints)
+        var startLatLon = getStartLatLon(laps)
         return new route(
             startTime,
             json.TrainingCenterDatabase.Activities.Activity.Creator.Name,
@@ -151,20 +135,19 @@ exports.convert = function (data) {
             utils.kmh(totalDistance, utils.durationInHours(startTime, endTime)),
             utils.minkm(totalDistance, utils.durationInMinutes(startTime, endTime)),
             geoPoints
-        );
-            
+        )
     }
-    
-    return new Promise(function(resolve, reject) {    
+
+    return new Promise(function(resolve, reject) {
         xml2js.parseStringAsync(data, { explicitArray: false, ignoreAttrs: true })
             .then(function (result) {
-                var json = result;
-                var route = processJson(json);
-                return resolve(route);
+                var json = result
+                var route = processJson(json)
+                return resolve(route)
             })
             .catch(function (err) {
-                console.log(err);
-                return reject();
-            });
-    });
-};
+                console.log(err)
+                return reject()
+            })
+    })
+}
--- spec/tcxConverterSpec.js
+++ spec/tcxConverterSpec.js
@@ -1,116 +1,113 @@
-var fs = require('fs');
-var path = require('path');
-var converter = require('../server/tcxConverter');
+var fs = require('fs')
+var path = require('path')
+var converter = require('../server/tcxConverter')
 
 var readFileAsString = function (relPath) {
-    return fs.readFileSync(path.join(__dirname, relPath), { encoding: 'utf8' });
+    return fs.readFileSync(path.join(__dirname, relPath), { encoding: 'utf8' })
 }
 
 describe("Tcx2Converter", function() {
-    it("should convert fitbit tcx data correctly", function(done) {
-        var relativePathFitbitSurge = 'data/993568829.tcx';
-        var fitbitBuffer = readFileAsString(relativePathFitbitSurge);
+    it("should convert Fitbit tcx data correctly", function(done) {
+        var relativePathFitbitSurge = 'data/993568829.tcx'
+        var fitbitBuffer = readFileAsString(relativePathFitbitSurge)
         converter.convert(fitbitBuffer).then(function(route) {
-
-            expect(route.startTime).toBe("2015-11-30T21:51:29.000+02:00");
-            expect(route.device).toBe("Fitbit Surge");
-            expect(route.dataPoints.length).toBe(2502);
-            expect(route.geoPoints.length).toBe(2502);
-            var dataPointCount = route.dataPoints.length;
-            expect(route.distance).toBe(5438.1);
-            expect(route.geoPoints[2498].lat).toBe(60.18599021434784);
-            expect(route.geoPoints[2498].lng).toBe(25.05650023619334);
-            expect(route.startLat).toBe(60.18615917364756);
-            expect(route.startLng).toBe(25.056412513057392);
-            expect(route.duration).toBe("41:42"); // 22:33:11 - 21:51:29
-            expect(route.kmh).toBe(7.8);
-            expect(route.minkm).toBe(7.7);
-            expect(route.dataPoints[4].timeStamp).toBe("2015-11-30T21:51:43.000+02:00");
-            expect(route.dataPoints[10].altitude).toBe("8.1");
-            expect(route.dataPoints[10].kmh).toBe(4.5);
-            expect(route.dataPoints[10].minkm).toBe(13.3);
-            expect(route.dataPoints[2499].heartRate).toBe("137");
-            expect(route.dataPoints[3].duration).toBe("0:13"); // 21:51:29 - 21:51:42
-            expect(route.dataPoints[0].percentage).toBe(0);
-            expect(route.dataPoints[1].percentage).toBeCloseTo(0.064, 3); // 3.5 / 5438.1 * 100
-            expect(route.dataPoints[2501].percentage).toBe(100);
+            expect(route.startTime).toBe("2015-11-30T21:51:29.000+02:00")
+            expect(route.device).toBe("Fitbit Surge")
+            expect(route.dataPoints.length).toBe(2502)
+            expect(route.geoPoints.length).toBe(2502)
+            var dataPointCount = route.dataPoints.length
+            expect(route.distance).toBe(5438.1)
+            expect(route.geoPoints[2498].lat).toBe(60.18599021434784)
+            expect(route.geoPoints[2498].lng).toBe(25.05650023619334)
+            expect(route.startLat).toBe(60.18615917364756)
+            expect(route.startLng).toBe(25.056412513057392)
+            expect(route.duration).toBe("41:42") // 22:33:11 - 21:51:29
+            expect(route.kmh).toBe(7.8)
+            expect(route.minkm).toBe(7.7)
+            expect(route.dataPoints[4].timeStamp).toBe("2015-11-30T21:51:43.000+02:00")
+            expect(route.dataPoints[10].altitude).toBe("8.1")
+            expect(route.dataPoints[10].kmh).toBe(4.5)
+            expect(route.dataPoints[10].minkm).toBe(13.3)
+            expect(route.dataPoints[2499].heartRate).toBe("137")
+            expect(route.dataPoints[3].duration).toBe("0:13") // 21:51:29 - 21:51:42
+            expect(route.dataPoints[0].percentage).toBe(0)
+            expect(route.dataPoints[1].percentage).toBeCloseTo(0.064, 3) // 3.5 / 5438.1 * 100
+            expect(route.dataPoints[2501].percentage).toBe(100)
             
             for (i=1; i < dataPointCount; i++) {
-                expect(route.dataPoints[i].percentage).not.toBeLessThan(route.dataPoints[i-1].percentage);
-                expect(route.dataPoints[i].duration).not.toBe("");
-                expect(route.dataPoints[i].distance).not.toBe("");
-                expect(route.dataPoints[i].altitude).not.toBe("");
-                expect(route.dataPoints[i].heartRate).not.toBe("");
-                var currentDate = new Date(route.dataPoints[i].timeStamp);
-                var previousDate = new Date(route.dataPoints[i-1].timeStamp);
-                expect(currentDate).not.toBeLessThan(previousDate);
+                expect(route.dataPoints[i].percentage).not.toBeLessThan(route.dataPoints[i-1].percentage)
+                expect(route.dataPoints[i].duration).not.toBe("")
+                expect(route.dataPoints[i].distance).not.toBe("")
+                expect(route.dataPoints[i].altitude).not.toBe("")
+                expect(route.dataPoints[i].heartRate).not.toBe("")
+                var currentDate = new Date(route.dataPoints[i].timeStamp)
+                var previousDate = new Date(route.dataPoints[i-1].timeStamp)
+                expect(currentDate).not.toBeLessThan(previousDate)
             }
-            done();
+            done()
         })
         .catch(function (e) {
             fail("Failed to convert Fitbit TCX XML to JSON.")
-            done();
-        });;          
-    });
+            done()
+        })
+    })
 
-    it("should convert garmin tcx data correctly", function(done) {
-        var relativePathGarminForerunner210 = 'data/activity_986153810.tcx';
-        var garminBuffer = readFileAsString(relativePathGarminForerunner210);
+    it("should convert Garmin tcx data correctly", function(done) {
+        var relativePathGarminForerunner210 = 'data/activity_986153810.tcx'
+        var garminBuffer = readFileAsString(relativePathGarminForerunner210)
         converter.convert(garminBuffer).then(function(route) {
-
-            expect(route.startTime).toBe("2015-12-10T18:03:58.000Z");
-            expect(route.device).toBe("Garmin Forerunner 210");
-            expect(route.dataPoints.length).toBe(608);
-            expect(route.geoPoints.length).toBe(608);
-            var dataPointCount = route.dataPoints.length;
-            expect(route.distance).toBe(8370.21);
-            expect(route.geoPoints[607].lat).toBe(60.991019094362855);
-            expect(route.geoPoints[607].lng).toBe(25.475816605612636);
-            expect(route.startLat).toBe(60.99109562113881);
-            expect(route.startLng).toBe(25.475734379142523);
-            expect(route.duration).toBe("52:21"); // 18:56:19 - 18:03:58
-            expect(route.kmh).toBe(9.6);
-            expect(route.minkm).toBe(6.3);
-            expect(route.dataPoints[4].timeStamp).toBe("2015-12-10T18:04:26.000Z");
-            expect(route.dataPoints[10].altitude).toBe("148.8000030517578");
-            expect(route.dataPoints[10].kmh).toBe(9.5);
-            expect(route.dataPoints[10].minkm).toBe(6.3);
-            expect(route.dataPoints[607].heartRate).toBe("");
-            expect(route.dataPoints[3].duration).toBe("0:14"); // 18:04:12 - 18:03:58        
-            expect(route.dataPoints[0].percentage).toBeCloseTo(0.033, 3); // 2.799999952316284 / 8370.2099609375 * 100
-            expect(route.dataPoints[607].percentage).toBe(100);
+            expect(route.startTime).toBe("2015-12-10T18:03:58.000Z")
+            expect(route.device).toBe("Garmin Forerunner 210")
+            expect(route.dataPoints.length).toBe(608)
+            expect(route.geoPoints.length).toBe(608)
+            var dataPointCount = route.dataPoints.length
+            expect(route.distance).toBe(8370.21)
+            expect(route.geoPoints[607].lat).toBe(60.991019094362855)
+            expect(route.geoPoints[607].lng).toBe(25.475816605612636)
+            expect(route.startLat).toBe(60.99109562113881)
+            expect(route.startLng).toBe(25.475734379142523)
+            expect(route.duration).toBe("52:21") // 18:56:19 - 18:03:58
+            expect(route.kmh).toBe(9.6)
+            expect(route.minkm).toBe(6.3)
+            expect(route.dataPoints[4].timeStamp).toBe("2015-12-10T18:04:26.000Z")
+            expect(route.dataPoints[10].altitude).toBe("148.8000030517578")
+            expect(route.dataPoints[10].kmh).toBe(9.5)
+            expect(route.dataPoints[10].minkm).toBe(6.3)
+            expect(route.dataPoints[607].heartRate).toBe("")
+            expect(route.dataPoints[3].duration).toBe("0:14") // 18:04:12 - 18:03:58        
+            expect(route.dataPoints[0].percentage).toBeCloseTo(0.033, 3) // 2.799999952316284 / 8370.2099609375 * 100
+            expect(route.dataPoints[607].percentage).toBe(100)
 
             for (i=1; i < dataPointCount; i++) {
-                expect(route.dataPoints[i].percentage).not.toBeLessThan(route.dataPoints[i-1].percentage);
-                expect(route.dataPoints[i].duration).not.toBe("");
-                expect(route.dataPoints[i].distance).not.toBe("");
-                expect(route.dataPoints[i].altitude).not.toBe("");
-                expect(route.dataPoints[i].heartRate).toBe("");
-                var currentDate = new Date(route.dataPoints[i].timeStamp);
-                var previousDate = new Date(route.dataPoints[i-1].timeStamp);
-                expect(currentDate).not.toBeLessThan(previousDate);
+                expect(route.dataPoints[i].percentage).not.toBeLessThan(route.dataPoints[i-1].percentage)
+                expect(route.dataPoints[i].duration).not.toBe("")
+                expect(route.dataPoints[i].distance).not.toBe("")
+                expect(route.dataPoints[i].altitude).not.toBe("")
+                expect(route.dataPoints[i].heartRate).toBe("")
+                var currentDate = new Date(route.dataPoints[i].timeStamp)
+                var previousDate = new Date(route.dataPoints[i-1].timeStamp)
+                expect(currentDate).not.toBeLessThan(previousDate)
             }
-            done();
+            done()
         })
         .catch(function (e) {
             fail("Failed to convert Garmin TCX XML to JSON.")
-            done();
-        });          
-        
-    });
+            done()
+        })
+    })
 
-    it("should convert tcx data with only one lap correctly", function(done) {
-        var relativePathFitbitSurge = 'data/1998685994.tcx';
-        var fitbitBuffer = readFileAsString(relativePathFitbitSurge);
+    it("should convert Tcx data with only one lap correctly", function(done) {
+        var relativePathFitbitSurge = 'data/1998685994.tcx'
+        var fitbitBuffer = readFileAsString(relativePathFitbitSurge)
         converter.convert(fitbitBuffer).then(function(route) {
-            expect(route.device).toBe("Fitbit Surge");
-            expect(route.distance).toBe(4780.29);
-            expect(route.startTime).toBe("2016-03-20T21:30:36.000+02:00");
-            done();
+            expect(route.device).toBe("Fitbit Surge")
+            expect(route.distance).toBe(4780.29)
+            expect(route.startTime).toBe("2016-03-20T21:30:36.000+02:00")
+            done()
         })
         .catch(function (e) {
-            fail("Failed to convert Fitbit TCX XML to JSON.")
-            done();
-        });
-    });
-});
+            fail("Failed to convert Fitbit TCX XML with only one lap to JSON.")
+            done()
+        })
+    })
+})
