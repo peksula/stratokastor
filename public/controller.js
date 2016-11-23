99fcbb395e293ffb620394d73e3cdc4f17e9c72b
Pekka Tukiainen <pekka.tukiainen@philips.com>
2016-11-23 21:29:21 +0200

    minor ui layout changes

--- public/controller.js
+++ public/controller.js
@@ -1,55 +1,6 @@
-var kastor = angular.module('kastor', ['ngMap']);
-var timer;
+var kastor = angular.module('kastor', ['ngMap'])
+var timer
 
-kastor.service('RouteService', ['$http', function($http){
-
-    this.getAll = function(successCallback, errorCallback) {
-        $http({
-            method: 'GET',
-            url: '/routes'
-        }).then(successCallback, errorCallback);
-    };
-
-    this.get = function(id, successCallback, errorCallback) {
-        $http({
-            method: 'GET',
-            url: '/routes/' + id
-        }).then(successCallback, errorCallback);
-    };
-
-    this.update = function(id, title, comment, weather, successCallback, errorCallback) {
-        $http({
-            method: 'POST',
-            url: '/routes/' + id,
-            data: {
-                title: title,
-                comment: comment,
-                weather: weather
-                }
-        }).then(successCallback, errorCallback);
-    };
-    
-    this.del = function(id, successCallback, errorCallback) {
-        $http({
-            method: 'DELETE',
-            url: '/routes/' + id,
-        }).then(successCallback, errorCallback)
-    };
-
-}]);
-
-kastor.service('TimeAndSpace', [function(){
-
-    this.millisecondsToNextPoint = function (currentTimestamp, nextTimestamp) {
-        var startDate = new Date(currentTimestamp);
-        var endDate = new Date(nextTimestamp);
-        var diffInMilliseconds = endDate.getTime() - startDate.getTime();
-        return diffInMilliseconds;
-    }
-
-
-}]);
-    
 kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', 'TimeAndSpace', function($scope, $timeout, RouteService, TimeAndSpace){
     var refreshRoutes = function(response) {
         $scope.routes = response.data;
