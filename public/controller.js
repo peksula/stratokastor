853b5ee57921a80ccf6c91b3d5b6c2ac75dd1963
Pekka Tukiainen <pekka.tukiainen@philips.com>
2016-11-23 22:03:52 +0200

    tuning ui controller

--- public/controller.js
+++ public/controller.js
@@ -38,35 +38,35 @@ kastor.service('RouteService', ['$http', function($http){
 
 }])
 
-kastor.service('TimeAndSpace', [function(){
+kastor.service('TimeUtils', [function(){
     this.millisecondsToNextPoint = function (currentTimestamp, nextTimestamp) {
-        var startDate = new Date(currentTimestamp);
-        var endDate = new Date(nextTimestamp);
-        var diffInMilliseconds = endDate.getTime() - startDate.getTime();
-        return diffInMilliseconds;
+        var startDate = new Date(currentTimestamp)
+        var endDate = new Date(nextTimestamp)
+        var diffInMilliseconds = endDate.getTime() - startDate.getTime()
+        return diffInMilliseconds
     }
 }])
 
-kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', 'TimeAndSpace', function($scope, $timeout, RouteService, TimeAndSpace){
+kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', 'TimeUtils', function($scope, $timeout, RouteService, TimeUtils){
     var refreshRoutes = function(response) {
-        $scope.routes = response.data;
-        console.log($scope.routes);
+        $scope.routes = response.data
+        console.log($scope.routes)
     }
 
     var refreshRoute = function(response) {
         if (timer !== undefined) {
-            $timeout.cancel(timer);
+            $timeout.cancel(timer)
         }
-        $scope.editorEnabled = false;
-        $scope.playbackToggled = false;
-        $scope.playbackMultiplier = 1;
-        $scope.route = angular.fromJson(response.data);
+        $scope.editorEnabled = false
+        $scope.playbackToggled = false
+        $scope.playbackMultiplier = 1
+        $scope.route = angular.fromJson(response.data)
         $scope.mapPosition = {
             lat: $scope.route.data.startLat,
             lng: $scope.route.data.startLng,
             zoom: 16
-        };
-        console.log($scope.route);
+        }
+        console.log($scope.route)
     }
 
     $scope.init = function() {
@@ -74,87 +74,87 @@ kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', 'Time
             lat: 60.6333,
             lng: 24.8500,
             zoom: 8
-        };
-        $scope.playbackMultiplier = 1;
-        $scope.editorEnabled = false;
-        $scope.playbackToggled = false;
-        
+        }
+        $scope.playbackMultiplier = 1
+        $scope.editorEnabled = false
+        $scope.playbackToggled = false
+
         RouteService.getAll(
             function successCallback(response) {
-                refreshRoutes(response);
+                refreshRoutes(response)
                 if ($scope.routes.length > 0) {
                     // Show the details of the first route
-                    $scope.showRoute($scope.routes[0]._id);
+                    $scope.showRoute($scope.routes[0]._id)
                 }
              },
             function errorCallback(response) {
-                console.log("Failed to load routes. %s.", response);
+                console.log("Failed to load routes. %s.", response)
             }
-        );
-    };
+        )
+    }
 
     $scope.showRoute = function(id) {
         RouteService.get(
             id,
             function successCallback(response) {
-                refreshRoute(response);
+                refreshRoute(response)
              },
             function errorCallback(response) {
-                console.log("Failed to load the route. %s.", response);
+                console.log("Failed to load the route. %s.", response)
             }
-        );
-	};    
+        )
+    }
 
     $scope.deleteRoute = function(id) {
-        $scope.editorEnabled = false;
+        $scope.editorEnabled = false
         RouteService.del(
             id,
             function successCallback(response) {
-                refreshRoutes(response);
-                $scope.route = "";
+                refreshRoutes(response)
+                $scope.route = ""
              },
             function errorCallback(response) {
-                console.log("Failed to delete the route. %s.", response);
+                console.log("Failed to delete the route. %s.", response)
             }
-        );
-	};
-    
+        )
+    }
+
     $scope.updateRoute = function(id){
-        $scope.editorEnabled = false;
+        $scope.editorEnabled = false
         RouteService.update(
             id,
             $scope.route.title,
             $scope.route.comment,
             $scope.route.weather,
             function successCallback(response) {
-                refreshRoutes(response);
+                refreshRoutes(response)
              },
             function errorCallback(response) {
-                console.log("Failed to update the route. %s.", response);
+                console.log("Failed to update the route. %s.", response)
             }
-        );
-    };
-    
+        )
+    }
+
     $scope.enableEditor = function() {
-        $scope.editorEnabled = true;
-        this.title = $scope.route.title;
-        this.comment = $scope.route.comment;
-        this.weather = $scope.route.weather;
-    };
-    
+        $scope.editorEnabled = true
+        this.title = $scope.route.title
+        this.comment = $scope.route.comment
+        this.weather = $scope.route.weather
+    }
+
     $scope.cancelEdit = function(id) {
-        $scope.editorEnabled = false;
-        $scope.route.title = this.title;
-        $scope.route.comment = this.comment;
-        $scope.route.weather = this.weather;
-    };
+        $scope.editorEnabled = false
+        $scope.route.title = this.title
+        $scope.route.comment = this.comment
+        $scope.route.weather = this.weather
+    }
 
-}]);
+}])
 
-kastor.controller('routeVisualizationController', function(NgMap, $scope, $timeout, TimeAndSpace) {
-    var vc = this;
-    var shape;
-    var marker;
+kastor.controller('routeVisualizationController', function(NgMap, $scope, $timeout, TimeUtils) {
+    var vc = this
+    var shape
+    var marker
     
     $scope.initMap = function() {
         NgMap.getMap().then(function(map) {
@@ -212,14 +212,14 @@ kastor.controller('routeVisualizationController', function(NgMap, $scope, $timeo
             if (i < $scope.route.data.dataPoints.length) {
                 var currentTimeStamp = $scope.route.data.dataPoints[i-1].timeStamp;
                 var nextTimeStamp = $scope.route.data.dataPoints[i].timeStamp;
-                var delay = TimeAndSpace.millisecondsToNextPoint(currentTimeStamp, nextTimeStamp);
+                var delay = TimeUtils.millisecondsToNextPoint(currentTimeStamp, nextTimeStamp);
                 delay = delay / $scope.$parent.playbackMultiplier;
                 timer = $timeout(step, delay);
             }
         }
         
         timer = $timeout(step, 250);
-	};
+    };
     
     $scope.stopRoute = function() {
         reset();
