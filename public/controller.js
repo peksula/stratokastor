97c090b2adc4f600e4592a519f6ade777916581a
Pekka Tukiainen <pekka.tukiainen@philips.com>
2016-11-23 21:50:45 +0200

    minor ui layout changes

--- public/controller.js
+++ public/controller.js
@@ -1,6 +1,42 @@
 var kastor = angular.module('kastor', ['ngMap'])
 var timer
 
+kastor.service('RouteService', ['$http', function($http){
+
+    this.getAll = function(successCallback, errorCallback) {
+        $http({
+            method: 'GET',
+            url: '/routes'
+        }).then(successCallback, errorCallback)
+    }
+
+    this.get = function(id, successCallback, errorCallback) {
+        $http({
+            method: 'GET',
+            url: '/routes/' + id
+        }).then(successCallback, errorCallback)
+    }
+
+    this.update = function(id, title, comment, weather, successCallback, errorCallback) {
+        $http({
+            method: 'POST',
+            url: '/routes/' + id,
+            data: {
+                title: title,
+                comment: comment,
+                weather: weather
+                }
+        }).then(successCallback, errorCallback)
+    }
+
+    this.del = function(id, successCallback, errorCallback) {
+        $http({
+            method: 'DELETE',
+            url: '/routes/' + id,
+        }).then(successCallback, errorCallback)
+    }
+}])
+
 kastor.controller('mainController', ['$scope', '$timeout', 'RouteService', 'TimeAndSpace', function($scope, $timeout, RouteService, TimeAndSpace){
     var refreshRoutes = function(response) {
         $scope.routes = response.data;
