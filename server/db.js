f23fa5279d3b4dc981f7743a2e544f3741fd83e1
Pekka Tukiainen <pekka.tukiainen@philips.com>
2017-01-08 11:09:08 +0200

    fixing variable declaration

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
