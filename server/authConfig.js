628741be723bb7eb51e8dac0d41cc1189ad6532b
Pekka Tukiainen <pekka.tukiainen@philips.com>
2017-04-09 21:15:12 +0300

    adding oauth keys

---
+++ server/authConfig.js
@@ -0,0 +1,13 @@
+exports.secrets = function(config) {
+    const production = {
+        clientID: "918728042559-7vplh1m9qanjp7vpdj8ul58mqkqls837.apps.googleusercontent.com",
+        clientSecret: "2nFXu4DGfzosbCtp2_NhQBji",
+        callbackURL: "http://strato-kastor.rhcloud.com/auth/google/callback"
+    }
+    if (config == 'production') {
+        return production
+    }
+    else {
+        return null
+    }
+};
---
+++ spec/authConfigSpec.js
@@ -0,0 +1,15 @@
+var ac = require('../server/authConfig')
+
+describe("AuthConfig", function() {
+    it("should return production configuration when such is requested", function() {
+        var secrets = ac.secrets('production')
+        expect(secrets.clientID).toEqual("918728042559-7vplh1m9qanjp7vpdj8ul58mqkqls837.apps.googleusercontent.com")
+        expect(secrets.clientSecret).toEqual("2nFXu4DGfzosbCtp2_NhQBji")
+        expect(secrets.callbackURL).toEqual("http://strato-kastor.rhcloud.com/auth/google/callback")
+    })
+
+    it("should return null otherwise", function() {
+        var secrets = ac.secrets('localhost')
+        expect(secrets).toEqual(null)
+    })
+})
