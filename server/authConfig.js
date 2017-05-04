458a497828c52298eb24102ab4a766f7c54cb69f
Pekka Tukiainen <pekka.tukiainen@philips.com>
2017-05-04 09:22:50 +0300

    reading oauth client secret from environment variables

--- server/authConfig.js
+++ server/authConfig.js
@@ -1,7 +1,7 @@
 exports.secrets = function(config) {
     const production = {
         clientID: "918728042559-7vplh1m9qanjp7vpdj8ul58mqkqls837.apps.googleusercontent.com",
-        clientSecret: "2nFXu4DGfzosbCtp2_NhQBji",
+        clientSecret: process.env.CLIENT_SECRET,
         callbackURL: "http://strato-kastor.rhcloud.com/auth/google/callback"
     }
     if (config == 'production') {
@@ -10,4 +10,4 @@ exports.secrets = function(config) {
     else {
         return null
     }
-};
+}
